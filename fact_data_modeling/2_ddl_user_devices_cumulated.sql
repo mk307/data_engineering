130a5bf9f4d44399f6dcf74d79418d1d9461eaea
mk307 <115074350+mk307@users.noreply.github.com>
2025-04-22 12:29:04 +0500

    Delete fact_data_modeling directory

--- fact_data_modeling/2_ddl_user_devices_cumulated.sql
+++
@@ -1,9 +0,0 @@
-/* Task 2: A DDL for an user_devices_cumulated table */
-
-CREATE TABLE user_devices_cumulated (
-    user_id NUMERIC,
-    curr_date DATE,
-    browser_type TEXT,
-    device_activity_datelist DATE[],
-    PRIMARY KEY (user_id, browser_type, curr_date)
-);
